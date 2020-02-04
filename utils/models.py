1a706bff828262f3f7a4577888e2fc273808a943
yorch <sanchezenrique580@gmail.com>
2020-02-03 23:50:07 -0400

    Aplicacion base

---
+++ utils/models.py
@@ -0,0 +1,27 @@
+""" Model general """
+
+# Django
+from django.db import models
+
+
+class SpotiModel(models.Model):
+    """Modelo base que heredaran las aplicaciones
+
+    Este Modelos contendra campos para el fecha de creacion y modificacion
+    """
+    created = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Fecha y hora en la cual se modifico el objeto'
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+        help_text='Fecha y hora en la cual se modifico el objeto'
+    )
+
+    class Meta:
+        """Clase Meta."""
+        abstract = True
+        # Especificamos que el ultimo en traer de los querys sera el ultimo creado
+        get_latest_by = 'created'
+        ordering = ['-created', '-modified']  # Colocamos que los objetos del modelo seran devueltos
+        # ordenas en forma descendente por creacion, en le caso que haya coincidencia usara el de la modificacion
