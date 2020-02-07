f0c1a181e12abecd3488b545904e7b3690049bdd
yorch <sanchezenrique580@gmail.com>
2020-02-06 23:58:11 -0400

    Correcciones al modelo de albums

--- albums/models/albums.py
+++ albums/models/albums.py
@@ -10,21 +10,21 @@ from utils.models import SpotiModel
 class Album(SpotiModel):
     """Modelo de Album"""
 
-    title=models.CharField(
+    title = models.CharField(
         'ttulo del album',
         unique=True,
         max_length=60
     )
-    artist=models.ForeignKey(
+    artist = models.ForeignKey(
         'artists.Artist',on_delete=models.CASCADE,
         help_text='Artista que pertenece el album',
     )
-    description=models.TextField(
+    description = models.TextField(
         help_text='Descripcion del Album',
         max_length=250,
         null=True, blank=True
     )
-    year=models.PositiveSmallIntegerField(
+    year = models.PositiveSmallIntegerField(
         'year',
         default=0,
         help_text='Anio de publicacion de album'
@@ -33,4 +33,12 @@ class Album(SpotiModel):
     'foto de portada',
     upload_to='albums/portadas/',
     blank=True, null=True
-)
+    )
+    
+    def __str__(self):
+        """Retorna titulo del album mas su Artista."""
+        return 'Album {} por {}'.format(
+            self.title,
+            self.artist.name
+        )
+
