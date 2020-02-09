4e76ee9ed24d4bc6e04fa9adf847a044540bce14
yorch <sanchezenrique580@gmail.com>
2020-02-09 17:29:46 -0400

    Admin de Artistas

---
+++ artists/admin.py
@@ -0,0 +1,16 @@
+"""Admin de Artista."""
+
+# Django
+from django.contrib import admin
+
+# Model
+from artists.models import Artist
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    """Admin de Artistas."""
+    list_display = ('id', 'name', 'image', 'description')
+    list_display_links=('id','name')
+    search_fields = ('name', )
+    list_filter = ('created', 'modified')
+    list_editable = ('image', )
