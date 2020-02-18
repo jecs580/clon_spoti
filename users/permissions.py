49f42aba1cc3671d87ed05e5f0e62032a4aef6a5
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-02-17 20:39:30 -0400

    Agregando permiso para que solo los dueños de la cuenta puedan ver o actualizar sus datos

---
+++ users/permissions.py
@@ -0,0 +1,11 @@
+"""Permisos de Usuario"""
+
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+class IsAccountOwner(BasePermission):
+    """Permite acceso solo a los objetos propiedad del usuario solicitante"""
+
+    def has_object_permission(self,request,view,obj):
+        """Comprueba que obj y usuario son iguales"""
+        return request.user == obj 
